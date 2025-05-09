d65763d7217a7849698389541a3a11b60989f120
Cristian Fleita <cristianagustinfleita@gmail.com>
2025-05-09 17:03:17 -0300

    chore: vercel json

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import topLevelAwait from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
+import fs from "fs";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
@@ -11,5 +13,12 @@ export default defineConfig(() => {
     build: {
       outDir: "dist",
     },
+    // server: {
+    //   // host: "0.0.0.0", // or your local network IP
+    //   https: {
+    //     key: fs.readFileSync(path.resolve(__dirname, "localhost+2-key.pem")),
+    //     cert: fs.readFileSync(path.resolve(__dirname, "localhost+2-cert.pem")),
+    //   },
+    // },
   };
 });
