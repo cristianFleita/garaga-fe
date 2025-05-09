4185769a9572102da5ae4a33b945e82debaad49f
Cristian Fleita <cristianagustinfleita@gmail.com>
2025-05-09 08:50:18 -0300

    chore: update manifest

--- src/dojo/controller/controller.ts
+++ src/dojo/controller/controller.ts
@@ -37,7 +37,7 @@ const controllerOptions: ControllerOptions = {
   chains: [{ rpcUrl: RPC_URL }],
   defaultChainId,
   namespace: DOJO_NAMESPACE,
-  preset: "jokers-of-neon",
+  // preset: "jokers-of-neon",
 };
 
 if (CHAIN !== "mainnet" && CHAIN !== "sepolia") {
