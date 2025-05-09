7bcd930424279c182ba2847768054392cabcd4fc
Cristian Fleita <cristianagustinfleita@gmail.com>
2025-05-09 13:26:52 -0300

    chore: fix build

--- src/mocks/gridMock.ts
+++ src/mocks/gridMock.ts
@@ -1,20 +1,20 @@
 import { GridCell, CellType } from "../types/GameGrid";
 
-export const mockGrid: GridCell[] = [
-    { type: CellType.SHEEP },
-    { type: CellType.SHEEP_DEAD },
-    { type: CellType.SHEEP },
-    { type: CellType.SHEEP_DEAD },
-    { type: CellType.SHEEP },
-    { type: CellType.SHEEP_DEAD },
-    { type: CellType.SHEEP_FAKE },
-    { type: CellType.SHEEP },
-    { type: CellType.SHEEP },
-    { type: CellType.SHEEP_DEAD },
-    { type: CellType.SHEEP },
-    { type: CellType.SHEEP },
-    { type: CellType.SHEEP },
-    { type: CellType.SHEEP_DEAD },
-    { type: CellType.SHEEP },
-    { type: CellType.SHEEP },
-  ];
+// export const mockGrid: GridCell[] = [
+//     { type: CellType.SHEEP },
+//     { type: CellType.SHEEP_DEAD },
+//     { type: CellType.SHEEP },
+//     { type: CellType.SHEEP_DEAD },
+//     { type: CellType.SHEEP },
+//     { type: CellType.SHEEP_DEAD },
+//     { type: CellType.SHEEP_FAKE },
+//     { type: CellType.SHEEP },
+//     { type: CellType.SHEEP },
+//     { type: CellType.SHEEP_DEAD },
+//     { type: CellType.SHEEP },
+//     { type: CellType.SHEEP },
+//     { type: CellType.SHEEP },
+//     { type: CellType.SHEEP_DEAD },
+//     { type: CellType.SHEEP },
+//     { type: CellType.SHEEP },
+//   ];
