6b4589a27e42e8d1de3937403ecb986600446cd2
Damian Piñones <pinonesdamian@gmail.com>
2025-05-19 14:53:42 -0300

    update bg

--- src/components/GameBackground.tsx
+++ src/components/GameBackground.tsx
@@ -11,7 +11,7 @@ export const GameBackground = ({ children }: GameBackgroundProps) => {
       flexDirection={"column"}
       width={"100%"}
       height={"100%"}
-      background={`url(images/bg/bg_02.png) center center / cover no-repeat`}
+      background={`url(images/bg/bg.png) center center / cover no-repeat`}
       gap={4}
       py={2}
       px={8}
